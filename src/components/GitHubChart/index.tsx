1f2422132a0c30d4778c88f9d8ced4e1f6d1ef5c
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-07-16 15:07:07 +0600

    Add click handler to square

--- src/components/GitHubChart/index.tsx
+++ src/components/GitHubChart/index.tsx
@@ -5,7 +5,12 @@ import getRussianMonthName from '@/utils/getRussianMonthName';
 function GitHubChart() {
   const [squares, setSquares] = useState<JSX.Element[]>([]);
   const [months, setMonths] = useState<string[]>([]);
+  const [selectedSquare, setSelectedSquare] = useState<number>(-1);
   const [error, setError] = useState<string>('');
+
+  const handleSquareClick = (index: number) => {
+    setSelectedSquare(index);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +27,16 @@ function GitHubChart() {
           const dateString = date.toISOString().split('T')[0];
 
           const level = data[dateString] ? calculateLevel(data[dateString]) : 1;
-          squaresArray.unshift(<li className="square" data-level={level}></li>);
+          const isSelected = selectedSquare === i;
+          const squareClass = isSelected ? 'square square_selected' : 'square';
+          squaresArray.unshift(
+            <li
+              key={i}
+              className={squareClass}
+              data-level={level}
+              onClick={() => handleSquareClick(i)}
+            ></li>
+          );
 
           if (i % 30 === 0) {
             const monthIndex = date.getMonth();
@@ -39,14 +53,12 @@ function GitHubChart() {
     };
 
     fetchData();
-  }, []);
+  }, [selectedSquare]);
 
   return (
     <div className="github-chart">
       {error ? (
-        <div className="error">
-          {error}
-        </div>
+        <div className="error">{error}</div>
       ) : (
         <div className="graph">
           <ul className="months">
